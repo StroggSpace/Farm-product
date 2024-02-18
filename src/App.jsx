0e8affa1baa8cfad09fb5022727c17d0d42390d4
Juriy_Strogg <Strog1998@gmail.com>
2024-02-18 19:11:34 +0500

    Advanced Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <PageWrapper>
+        <Header>
+          <Logo />
+          <Nav>
+            <Button />
+          </Nav>
+        </Header>
+        <MainPage>
+          <About>
+            <Title />
+          </About>
+          <ComparisonList>
+            <ComparisonItem />
+            <ComparisonItem />
+            <ComparisonItem />
+            <ComparisonItem />
+            <Button />
+          </ComparisonList>
+        </MainPage>
+        <Footer>
+          <Logo />
+        </Footer>
+      </PageWrapper>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ui/copyright/Copyright.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Copyright() {
+  return <></>;
+}
+
